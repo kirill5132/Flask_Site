cb4ef2ea8cae732e477c4a3ecc8b374412c57428
sn002 <sn0020068@gmail.com>
2024-03-29 09:30:48 +0300

    home

--- Flask_Master.py
+++ Flask_Master.py
@@ -25,5 +25,15 @@ def slovar():
     slovar0_name = 'sigma', 'sigma0'
     return render_template('slovar.html', sigma = slovar0_name)
 
+@app.route("/entrance/")
+def entrance():
+    entrance_name = 'подъезд 1'
+    return render_template('entrance.html', entrance = entrance_name)
+
+@app.route("/town/")
+def town():
+    town_name = 'Чебоксары'
+    return render_template('town.html', town = town_name)
+
 if __name__ == "__main__":
     app.run(debug=True)
